ce8db389d7ebe605af2d895c1b03a34974fc78df
unknown <Munendra@gmail.com>
2026-05-30 11:55:11 +0530

    SQL changes

--- infra/terraform/aks-monitoring.tf
+++ infra/terraform/aks-monitoring.tf
@@ -18,7 +18,7 @@ resource "azurerm_dashboard_grafana" "main" {
   name                              = "grafana-${local.prefix}"
   resource_group_name               = azurerm_resource_group.main.name
   location                          = azurerm_resource_group.main.location
-  grafana_major_version             = 9
+  grafana_major_version             = 11
   api_key_enabled                   = false
   deterministic_outbound_ip_enabled = true
   public_network_access_enabled     = true
